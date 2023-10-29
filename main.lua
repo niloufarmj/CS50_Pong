4a06c686311cd90004d33a2247439b5c3d873de1
Niloufar Moradi Jam <46811379+niloufarmj@users.noreply.github.com>
2023-10-29 21:53:20 +0330

    update ui + add ball and paddles

--- main.lua
+++ main.lua
@@ -3,14 +3,17 @@ push = require 'push'
 WINDOW = {
     Width = 1000,
     Height = 600,
-    VIRTUAL_WIDTH = 350,
-    VIRTUAL_HEIGHT = 200
+    VirtualWidth = 350,
+    VirtualHeight = 200
 }
 
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    push:setupScreen(WINDOW.VIRTUAL_WIDTH, WINDOW.VIRTUAL_HEIGHT, WINDOW.Width, WINDOW.Height, {
+    smallFont = love.graphics.newFont('ModernWarfare-OV7KP.ttf', 10)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(WINDOW.VirtualWidth, WINDOW.VirtualHeight, WINDOW.Width, WINDOW.Height, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -21,20 +24,35 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(35/255, 5/255, 65/255, 255/255)
+
     love.graphics.printf(
-        'My First Game with LOVE2D:',    
+        'pong game made by nanami',    
         0,                     
-        WINDOW.VIRTUAL_HEIGHT / 2 - 20,  
-        WINDOW.VIRTUAL_WIDTH,       
-        'center') 
-        
-    love.graphics.printf(
-        'PONG',    
-        0,                    
-        WINDOW.VIRTUAL_HEIGHT / 2 + 10,  
-        WINDOW.VIRTUAL_WIDTH,           
+        10,  
+        WINDOW.VirtualWidth,       
         'center') 
 
+    love.graphics.rectangle("fill", 5, 30, 340, 2)    
+    love.graphics.rectangle("fill", 5, 50, 340, 2)
+    love.graphics.rectangle("fill", WINDOW.VirtualWidth / 2 - 1, 30, 2, 20)  
+    
+    love.graphics.printf(
+        'Player 1 : 0',    
+        -90,                     
+        37.5,  
+        WINDOW.VirtualWidth,       
+        'center') 
+    love.graphics.printf(
+        'Player 2 : 0',    
+        90,                     
+        37.5,  
+        WINDOW.VirtualWidth,       
+        'center') 
+
+    love.graphics.rectangle('fill', 15, 70, 6, 25) -- left paddle
+    love.graphics.rectangle('fill', WINDOW.VirtualWidth - 20, WINDOW.VirtualHeight - 50, 6, 25) -- right paddle
+    love.graphics.circle("fill", WINDOW.VirtualWidth / 2 - 2.5, WINDOW.VirtualHeight / 2 + 27.5, 5) -- ball
     push:apply('end')
 end
 
